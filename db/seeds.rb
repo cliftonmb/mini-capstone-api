0415a540b54d5e9aa5d00ee91226cb2998bc1cb4
Clifton Butler <cliftonmbutler@gmail.com>
2022-04-20 12:52:47 -0500

    fix seeds file by changing title to name

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
 Product.create!([
-  {title: "Lunch Box", price: 23, image_url: "https://m.media-amazon.com/images/I/71GKRyt+r3L._AC_SY300_SX300_.jpg", description: "Really, super fancy lunch box for lunches.", in_stock: nil},
-  {title: nil, price: 1555, image_url: "https://images.pexels.com/photos/4846097/pexels-photo-4846097.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500", description: nil, in_stock: nil},
-  {title: "Figurine", price: 55, image_url: "http", description: "really strong material", in_stock: nil},
-  {title: "bird_cage", price: 55, image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTddkwUxzg1mlls3md0e6jmo8IJCepK4_hCPQ&usqp=CAU", description: "hold my birds", in_stock: nil},
-  {title: "Tim Anderson Poster", price: 35, image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWAdJFVDXAcwu0Nn6f1dW3O-k9UO3rIW3A2Q&usqp=CAU", description: "goes on your walls", in_stock: nil},
-  {title: "Raven's Couch", price: 1555, image_url: "https://images.pexels.com/photos/4846097/pexels-photo-4846097.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500", description: "Large, lovely, lucious couch", in_stock: nil},
-  {title: "Another Green Couch", price: 55, image_url: "https://cdn-images.article.com/products/SKU12684/2890x1500/image58197.jpg?fit=max&w=2600&q=60&fm=webp", description: "This couch is not as great as Raven's couch, but it is still a good buy.", in_stock: nil},
-  {title: "Another Green Couch", price: 5, image_url: "https://cdn-images.article.com/products/SKU12684/2890x1500/image58197.jpg?fit=max&w=2600&q=60&fm=webp", description: "This couch is not as great as Raven's couch, but it is still a good buy.", in_stock: nil},
-  {title: "big couch", price: 1555, image_url: "https://images.pexels.com/photos/4846097/pexels-photo-4846097.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500", description: "Large, lovely, lucious couch", in_stock: nil},
-  {title: "small shoe", price: 3, image_url: "url", description: "these are small shoes", in_stock: nil}
+  {name: "Lunch Box", price: 23, image_url: "https://m.media-amazon.com/images/I/71GKRyt+r3L._AC_SY300_SX300_.jpg", description: "Really, super fancy lunch box for lunches.", in_stock: nil},
+  {name: nil, price: 1555, image_url: "https://images.pexels.com/photos/4846097/pexels-photo-4846097.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500", description: nil, in_stock: nil},
+  {name: "Figurine", price: 55, image_url: "http", description: "really strong material", in_stock: nil},
+  {name: "bird_cage", price: 55, image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTddkwUxzg1mlls3md0e6jmo8IJCepK4_hCPQ&usqp=CAU", description: "hold my birds", in_stock: nil},
+  {name: "Tim Anderson Poster", price: 35, image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWAdJFVDXAcwu0Nn6f1dW3O-k9UO3rIW3A2Q&usqp=CAU", description: "goes on your walls", in_stock: nil},
+  {name: "Raven's Couch", price: 1555, image_url: "https://images.pexels.com/photos/4846097/pexels-photo-4846097.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500", description: "Large, lovely, lucious couch", in_stock: nil},
+  {name: "Another Green Couch", price: 55, image_url: "https://cdn-images.article.com/products/SKU12684/2890x1500/image58197.jpg?fit=max&w=2600&q=60&fm=webp", description: "This couch is not as great as Raven's couch, but it is still a good buy.", in_stock: nil},
+  {name: "Another Green Couch", price: 5, image_url: "https://cdn-images.article.com/products/SKU12684/2890x1500/image58197.jpg?fit=max&w=2600&q=60&fm=webp", description: "This couch is not as great as Raven's couch, but it is still a good buy.", in_stock: nil},
+  {name: "big couch", price: 1555, image_url: "https://images.pexels.com/photos/4846097/pexels-photo-4846097.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500", description: "Large, lovely, lucious couch", in_stock: nil},
+  {name: "small shoe", price: 3, image_url: "url", description: "these are small shoes", in_stock: nil}
 ])
